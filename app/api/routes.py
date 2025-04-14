06fb98eb1f706911097eff3d88a03dc31447038a
tensorwitz <niklas.witzel@tensora.co>
2025-04-14 12:17:47 +0200

    Renamed health check endpoint

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
-from app.api.endpoints import frontend_projects, health, predictions, images
+from app.api.endpoints import frontend_projects, health_check, predictions, images
 
 # Create the main router
 router = APIRouter()
 
 # Include routers from different modules
-router.include_router(health.router, prefix="/health", tags=["health"])
+router.include_router(health_check.router, prefix="/health", tags=["health"])
 router.include_router(
     frontend_projects.router, prefix="/frontend-projects", tags=["frontend-projects"]
 )
