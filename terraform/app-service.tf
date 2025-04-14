21bc804f741a1c1795b74359121d7064c098864a
tensorwitz <niklas.witzel@tensora.co>
2025-04-14 14:23:09 +0200

    corrected env variables

--- terraform/app-service.tf
+++ terraform/app-service.tf
@@ -73,7 +73,7 @@ resource "azurerm_linux_web_app" "count_backend" {
     COSMOS_DB_ENDPOINT                  = azurerm_cosmosdb_account.count.endpoint
     COSMOS_DB_PRIMARY_KEY               = azurerm_cosmosdb_account.count.primary_key
     COSMOS_DB_DATABASE_NAME             = azurerm_cosmosdb_sql_database.count.name
-    BLOB_CONNECTION_STRING              = data.azurerm_storage_account.count.primary_blob_connection_string
+    AZURE_STORAGE_CONNECTION_STRING     = data.azurerm_storage_account.count.primary_blob_connection_string
   }
 
   lifecycle {
