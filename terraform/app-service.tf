dbd4b4979d559ff7df34488d34c8579a3b435664
tensorwitz <niklas.witzel@tensora.co>
2025-05-25 16:05:02 +0300

    temporarily use old cosmos DB and storage account

--- terraform/app-service.tf
+++ terraform/app-service.tf
@@ -65,10 +65,14 @@ resource "azurerm_linux_web_app" "count_backend" {
     WEBSITES_PORT                       = 8000
     API_KEY                             = var.api_key
     LOG_LEVEL                           = var.log_level
-    COSMOS_DB_ENDPOINT                  = azurerm_cosmosdb_account.count.endpoint
-    COSMOS_DB_PRIMARY_KEY               = azurerm_cosmosdb_account.count.primary_key
-    COSMOS_DB_DATABASE_NAME             = azurerm_cosmosdb_sql_database.count.name
-    AZURE_STORAGE_CONNECTION_STRING     = azurerm_storage_account.count.primary_blob_connection_string
+    // COSMOS_DB_ENDPOINT                  = azurerm_cosmosdb_account.count.endpoint
+    COSMOS_DB_ENDPOINT                  = "https://count-cosmosdb.documents.azure.com:443/"
+    // COSMOS_DB_PRIMARY_KEY               = azurerm_cosmosdb_account.count.primary_key
+    COSMOS_DB_PRIMARY_KEY               = "USIglqkLgcni7rPGH5VaECPeXqgEQVCEa1fJ3fbUpMRLzp9rBRQFNWbMpxIASXRcKT9KHYqPXZnlACDbxXFy6g=="
+    // COSMOS_DB_DATABASE_NAME             = azurerm_cosmosdb_sql_database.count.name
+    COSMOS_DB_DATABASE_NAME             = "tensora-count"
+    // AZURE_STORAGE_CONNECTION_STRING     = azurerm_storage_account.count.primary_blob_connection_string
+    AZURE_STORAGE_CONNECTION_STRING     = "DefaultEndpointsProtocol=https;AccountName=tensoracountstorage;AccountKey=6VnG/vk5ZASZWwadPfOXFSrSILsjcOxS3ilOSsw+7zWGHLRDqHItHt7OtyZLozI02OIx1ZsT6FKl+AStUmwang==;EndpointSuffix=core.windows.net"
   }
 
   lifecycle {
